9629158477b15c05221a9cbbe6bca1f3dd404a4e
Tauqueer Alam <106817907+Strake42@users.noreply.github.com>
2023-02-25 16:10:36 +0530

    Update BankAccountManagementSystem.java

--- BankAccountManagementSystem.java
+++ BankAccountManagementSystem.java
@@ -5,6 +5,16 @@ class BankDetails {
     private String userName;
     private int balance;
     Scanner sc = new Scanner(System.in);
+    
+    public void openAccount() {
+        System.out.print("Enter Account No: ");
+        accountNumber = sc.next();
+        System.out.print("Enter Name: ");
+        userName = sc.next();
+        System.out.print("Enter Balance: ");
+        balance = sc.nextInt();
+    }
+    
 }
     
     
