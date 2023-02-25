1c97c4c6111c585c3577c84eec3e63acb2b8e867
Tauqueer Alam <106817907+Strake42@users.noreply.github.com>
2023-02-25 16:09:58 +0530

    Update BankAccountManagementSystem.java

--- BankAccountManagementSystem.java
+++ BankAccountManagementSystem.java
@@ -1,7 +1,16 @@
 import java.util.*;
+
+class BankDetails {
+    private String accountNumber;
+    private String userName;
+    private int balance;
+    Scanner sc = new Scanner(System.in);
+}
+    
+    
 public class BankAccountManagementSys {
     public static void main(String[] args) {
-        int n;
+        
         
     }
 }
